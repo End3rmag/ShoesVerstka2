d7959fc2f951ead755e6c2392fc8a67165190dd3
irreaaa <irispbmink@gmail.com>
2025-03-27 14:31:18 +0300

    321

---
+++ app/src/main/java/com/example/myapplication/ui/screen/Otp/OptScrn.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.screen.Otp
+
+import android.health.connect.datatypes.units.Length
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun OtpTextFiled(
+    length: Int,
+    onValueChanged:(String)->Unit,
+    value: String,
+    modifier: Modifier =Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    keyboardActions: KeyboardActions = KeyboardActions()
+){
+    BasicTextField(
+        length = length,
+        keyboardActions = keyboardActions,
+        keyboardOptions = keyboardOptions,
+        modifier = modifier,
+        onValueChanged = onValueChanged{
+            if(it.length<= length){
+                onValueChanged(it)
+            }
+        },
+
+
+
+    )
+}
